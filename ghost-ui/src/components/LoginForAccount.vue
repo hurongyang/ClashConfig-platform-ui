0db646eff4157588a88a5e8ab9bbeffff3a6bd88
胡荣扬 <rongyang.hu@longbridge.sg>
2021-07-09 22:25:22 +0800

    保存

---
+++ ghost-ui/src/components/LoginForAccount.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="account">
+    <el-button type="primary" round>{{ msg }}</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    msg: {
+      type: String,
+      default: '统一账号登录'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.account {
+  margin-top: 100px
+}
+.el-button {
+  width: 100%;
+}
+</style>
---
+++ ghost-ui/src/components/LoginForEmail.vue
@@ -0,0 +1,44 @@
+<template>
+<div>
+  <!-- 登录表单 -->
+  <el-form ref="form" :model="form">
+  <el-form-item>
+    <el-input
+    placeholder="请输入账号"
+    prefix-icon="el-icon-user-solid"
+    v-model="form.email"
+    >
+    </el-input>
+  </el-form-item>
+  <el-form-item>
+    <el-input
+    placeholder="请输入密码"
+    prefix-icon="el-icon-lock"
+    v-model="form.password"
+    >
+    </el-input>
+  </el-form-item>
+  </el-form>
+  <!-- 登录 -->
+  <el-button type="primary" round style="width: 100%">登录</el-button>
+</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      form: {
+        email: '',
+        password: ''
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.el-form {
+  padding: 25px 0;
+}
+</style>
